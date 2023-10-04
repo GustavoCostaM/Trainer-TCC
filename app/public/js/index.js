96e78bae623566c601e96a63e82ef1e7682f15e3
Gustavo Costa <gustavo.costa.moraes@gmail.com>
2023-10-04 19:05:55 -0300

    slide

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,14 +1,14 @@
 function slide1(){
-    document.getElementById(slideImg1).src="1.bmp";
+    document.getElementById('slideImg1').src="1.bmp";
     setTimeout("slide2()", 1000)
     }
     
-    function slide2(){
-    document.getElementById(slideImg2).src="2.bmp";
+function slide2(){
+    document.getElementById('slideImg2').src="2.bmp";
     setTimeout("slide3()", 1000)
     }
     
-    function slide3(){
-    document.getElementById(slideImg3).src="3.bmp";
+function slide3(){
+    document.getElementById('slideImg3').src="3.bmp";
     setTimeout("slide1()", 1000)
     }
