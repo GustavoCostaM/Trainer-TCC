ac0ee4b1f0d00b6603cf88caf74b1cdbb98b625e
Gustavo Costa <gustavo.costa.moraes@gmail.com>
2023-10-08 20:24:48 -0300

    Tel

--- app/public/js/cadastro.js
+++ app/public/js/cadastro.js
@@ -39,6 +39,18 @@ function cadastro(event) {
 
 }
 
+const handlePhone = (event) => {
+  let input = event.target
+  input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+  if (!value) return ""
+  value = value.replace(/\D/g,'')
+  value = value.replace(/(\d{2})(\d)/,"($1) $2")
+  value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+  return value
+}
 
 
 
