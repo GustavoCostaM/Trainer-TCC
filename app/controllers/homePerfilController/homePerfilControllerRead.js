8a6cd9c7da897a2ac5979b4493349a68ef10059c
Gustavo Costa <gustavo.costa.moraes@gmail.com>
2023-10-18 01:32:35 -0300

    Update homePerfilControllerRead.js

--- app/controllers/homePerfilController/homePerfilControllerRead.js
+++ app/controllers/homePerfilController/homePerfilControllerRead.js
@@ -5,13 +5,13 @@ class HomePerfilController {
     async getPage(req, res) {
         const token = req.session.token;
         const {userId} = jwt.decode(token, process.env.SECRET);
-        const user = await usuarioModel.findUserById(userId);
+        const usuario = await usuarioModel.findUserById(userId);
 
 
         return res.render("pages/perfil-user.ejs", {
             data: {
                 page_name: "Perfil",                
-                user
+                usuario
             }
         })
     }
